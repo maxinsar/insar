8cfc8b33e9a0ebbfa9c72c3b28b0848b27f4a03b
maxinsar <trmaxim@list.ru>
2019-09-16 21:21:52 +0300

    Version 2

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Company, Vacancy, Comments
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class CompanyView(View):
+
+    template_name = "company/list_company.html"
+
+    def get(self, request, *args, **kwargs):
+        companies = Company.objects.all()
+        return render(request, self.template_name, {'companies':companies})
